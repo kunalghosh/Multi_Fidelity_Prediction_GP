5b0649fcc75ed04478ad656784abcb8e1013d2f5
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-07-08 12:06:54 +0300

    Modified slurm script to create a build directory for build artifacts.

--- mfgp/task1/run_gp.sh
+++ mfgp/task1/run_gp.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $WRKDIR/MBTR_62k_GEN/Multi_Fidelity_Prediction_GP/mfgp/task1
-module load anaconda3
+mkdir build || true
+module load anaconda3 || true
 source activate ../task0/build_mbtr/myenv
 python GP_prediction_low_fidelity_ExactGPyTorch.py ../task0/mbtr_data_flattened/mbtr.npz ../task0/build_mbtr/df_full_split_ids_with_smiles_v15.json
