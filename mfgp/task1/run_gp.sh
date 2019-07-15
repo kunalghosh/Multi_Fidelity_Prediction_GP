ba124aef3aac3f6c533cf18f36ce36af07a05dd3
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-07-15 13:35:07 +0300

    [Bug Fix] change directory to build, before generating build artifacts.

--- mfgp/task1/run_gp.sh
+++ mfgp/task1/run_gp.sh
@@ -2,6 +2,7 @@
 
 cd $WRKDIR/MBTR_62k_GEN/Multi_Fidelity_Prediction_GP/mfgp/task1
 mkdir build || true
+cd build
 module load anaconda3 || true
 source activate ../task0/build_mbtr/myenv
 python GP_prediction_low_fidelity_ExactGPyTorch.py ../task0/mbtr_data_flattened/mbtr.npz ../task0/build_mbtr/df_full_split_ids_with_smiles_v15.json
