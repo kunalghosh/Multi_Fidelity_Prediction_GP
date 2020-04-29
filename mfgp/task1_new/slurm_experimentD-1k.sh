aefdca0d95f90ac74d3bbe070a456480b7712964
Kunal Ghosh <kunal.t2@gmail.com>
2020-04-29 23:11:32 +0300

    Removed account name when running under triton.

--- mfgp/task1_new/slurm_experimentD-1k.sh
+++ mfgp/task1_new/slurm_experimentD-1k.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=activeLearning-D-1k
-#SBATCH --account=project_2000382
+# SBATCH --account=project_2000382
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #  SBATCH --mem-per-cpu=5G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=30
-#SBATCH --partition=small
-#SBATCH --output=activeLearning-D-1k.out
+#SBATCH --partition=batch
+#SBATCH --output=activeLearning-D-1k-temp.out
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-module load bioconda/3
+module load anaconda/3
 export PROJAPPL=/projappl/project_2000382
 
 srun main-D-1k.sh
