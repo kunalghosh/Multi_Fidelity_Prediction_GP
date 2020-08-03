b11f5eb2e7e12da6d5bb6164e3c6d8009374045b
Kunal Ghosh <kunal.t2@gmail.com>
2020-08-03 15:39:05 +0300

    Fixed puhti scripts.

--- mfgp/task1_new/puhti_runs/AA_A_8k/main.sh
+++ mfgp/task1_new/puhti_runs/AA_A_8k/main.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh -e
 #source activate ../../task0/myenv
 #cd $1
-echo pwd
+echo `pwd`
 python ../../../__main__.py $2
--- mfgp/task1_new/puhti_runs/slurm_experiment_array_1k.sh
+++ mfgp/task1_new/puhti_runs/slurm_experiment_array_1k.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=activeLearning-A-4k
 #SBATCH --account=project_2000382
-#SBATCH --time=60:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mem-per-cpu=5G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --partition=large
-#SBATCH --array=1-20
-#SBATCH --output=activeLearning_%a.out
+#SBATCH --array=1-10
+#SBATCH --output=activeLearning_1k_%a.out
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 dirs=(
-"./AA_A_4k/run1"
-"./AA_A_4k/run2"
-"./AA_A_4k/run3"
-"./AA_A_4k/run4"
-"./AA_A_4k/run5"
-"./AA_A_8k/run1"
-"./AA_A_8k/run2"
-"./AA_A_8k/run3"
-"./AA_A_8k/run4"
-"./AA_A_8k/run5"
-"./AA_D_4k/run1"
-"./AA_D_4k/run2"
-"./AA_D_4k/run3"
-"./AA_D_4k/run4"
-"./AA_D_4k/run5"
-"./AA_D_8k/run1"
-"./AA_D_8k/run2"
-"./AA_D_8k/run3"
-"./AA_D_8k/run4"
-"./AA_D_8k/run5"
+"./AA_D_1k/run1"
+"./AA_D_1k/run2"
+"./AA_D_1k/run3"
+"./AA_D_1k/run4"
+"./AA_D_1k/run5"
+"./AA_A_1k/run1"
+"./AA_A_1k/run2"
+"./AA_A_1k/run3"
+"./AA_A_1k/run4"
+"./AA_A_1k/run5"
 )
 
 module load python-data/3.7.6-1
