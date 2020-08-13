abf493ec9700b7d882372e8e9df05143c10acec3
Kunal Ghosh <kunal.ghosh@aalto.fi>
2020-08-13 22:08:57 +0300

    Added a more updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="mfgp", version="0.1", packages=find_packages())
+setup(name="aldc", 
+      version="0.1", 
+      description="Python active learning code for dataset compilation.",
+      author="Kunal Ghosh",
+      author_email="kunal.ghosh@aalto.fi",
+      packages=find_packages())
