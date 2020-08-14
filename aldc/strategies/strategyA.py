6c18c9bcf12a77c0cc88caadf001534821b79143
emmalumi <emma.lumiaro@gmail.com>
2020-08-14 10:21:21 +0300

    Adding implementation to strategyA

--- aldc/strategies/strategyA.py
+++ aldc/strategies/strategyA.py
@@ -3,4 +3,5 @@ def strategy_A(heldout_set: list, batch_size: int, random_seed: int, debug=False
   Random strategy :
     1. Pick molecules randomly from the held out set.
   """
-  return heldout_set
+  prediction_set, heldout_set = pre_rem_split(batch_size, heldout_set, random_seed) 
+  return prediction_set
