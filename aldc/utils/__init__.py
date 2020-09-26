d29104767b0ef53b0af2b895129bffceb0f023df
Kunal Ghosh <kunal.ghosh@aalto.fi>
2020-09-26 06:16:23 +0300

    Added config loader to utils.

--- aldc/utils/__init__.py
+++ aldc/utils/__init__.py
@@ -2,3 +2,5 @@ from .timing import timer
 from .util import loadjson
 from .sorting import sort_and_get_last_x, sort, get_last_x
 from .clustering import cluster, get_closest_to_center
+from .config_class import Config
+from .config_loader import get_config_from_json
