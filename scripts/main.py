2280cd2ad46c3caca96b7c19d375080ae084e401
Kunal Ghosh <kunal.ghosh@aalto.fi>
2020-09-29 14:50:49 +0300

    added first draft of main class.

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
 import sys
 import numpy as np
 from aldc.utils import get_config_from_json
-from aldc.dataset import AADataset
+from aldc.utils import Metric
+from aldc.dataset import MaterialDataset
 from aldc.models import SKLearnGPModel
 
 config_file_path = sys.argv[1] # the config is the only argument
@@ -12,7 +13,7 @@ np.random.seed(config.random_seed)
 
 # load dataset
 # This should be generic (data should be pre-formatted, so we don't need custom loading logic)
-dataset = AADataset(feature_path=config.features_path, targets_path=config.targets_path)
+dataset = MaterialDataset(feature_path=config.features_path, targets_path=config.targets_path)
 
 # setup the model
 # control from config
@@ -27,20 +28,25 @@ data_handler = DataHandler(dataset, dataset_size = config.dataset_size,
                           batches_list = config.batch_sizes,
                           random_seed = config.random_seed)
 
+metric = Metric()
 # TODO : All strategies should take the same arguments so passing arguments is easier.
 strategy = StrategyGetter.get_strategy(config.acquisition_name)
 
 # active learning loop
-for iter, batch_size in enumerate(self.batches_list):
+for iter, batch_size in enumerate(data_handler): # iterates through batch_indices
 
     data_splits = data_handler.get_splits(iter)
 
     model.train(dataset[data_splits.train_indices]) # TODO : save model parameters in a list after each "train()" so we have model hyperparams
-    predictions = model.predict(dataset[data_splits.heldout_indices])
 
-    trainset_new_idxs = strategy(data_splits.heldout_set, predictions, batch_size, random_seed = config.random_seed)
+    train_predictions = model.predict(dataset[data_splits.train_indices])
+    test_predictions = model.predict(dataset[data_splits.test_indices])
+    # compute train and test predictions
+    metric.mae("train", train_predictions,dataset[data_splits.train_indices])
+    metric.mae("test", test_predictions,dataset[data_splits.test_indices])
+
+    heldout_predictions = model.predict(dataset[data_splits.heldout_indices])
+    trainset_new_idxs = strategy(data_splits.heldout_set, heldout_predictions, batch_size, random_seed = config.random_seed)
 
     # add the newly selected indices to the training set
-    data_splits.add_train_idxs(trainset_new_idxs)
-    # print MAE on the train set.
-    # print MAE on the test set.
+    data_splits.update_splits(trainset_new_idxs)
