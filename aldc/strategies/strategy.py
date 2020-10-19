fa9db44d90bf0604d0bb590b2dba85d38bce52ee
Kunal Ghosh <kunal.ghosh@aalto.fi>
2020-10-19 16:23:44 +0300

    Fixed a typo

--- aldc/strategies/strategy.py
+++ aldc/strategies/strategy.py
@@ -7,20 +7,18 @@ from aldc.strategies.strategyF import strategy_F
 from aldc.strategies.strategyG import strategy_G
 
 class StrategyGetter:
-    def __init__(self):
+    def __init__(self, logger=None):
         super(StrategyGetter, self).__init__()
-        self.strategy_A = strategy_A
-        self.strategy_B = strategy_B
-        self.strategy_C = strategy_C
-        self.strategy_D = strategy_D
-        self.strategy_E = strategy_E
-        self.strategy_F = strategy_F
-        self.strategy_G = strategy_G
-    
+        self.strategyA = strategy_A
+        self.strategyB = strategy_B
+        self.strategyC = strategy_C
+        self.strategyD = strategy_D
+        self.strategyE = strategy_E
+        self.strategyF = strategy_F
+        self.strategyG = strategy_G
+
     def get_strategy(self, name:str):
         if hasattr(self, name):
             return getattr(self, name)
         else:
             raise RuntimeError(f"Strategy with name {name} doesn't exist")
-
-
