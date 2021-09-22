91fff1d553ba4284a08bfdd7e46e53acd3ec6675
Kunal Ghosh <kunal.ghosh@aalto.fi>
2021-09-22 12:10:47 +0300

    referenced all InData objects directly from the class.

--- mfgp/task1_new/one_step_gp.py
+++ mfgp/task1_new/one_step_gp.py
@@ -83,47 +83,47 @@ def main():
   start_all = time.time()
   filepath = sys.argv[1]
   #>>> Load Config
-  InData = Input(filepath)
+  conf = Input(filepath)
 
-  #-- Output
-  out_name = InData.out_name
-  #-- Condition
-  fn_name = InData.fn_name
-  K_high = InData.K_high
-  rnd_size = InData.rnd_size
-  random_seed = InData.random_seed
-  #- Flag
-  save_load_flag = InData.save_load_flag
-  save_load_split_flag = InData.save_load_split_flag
-  restart_flag = InData.restart_flag
-  #-- Data set
-  dataset = InData.dataset
-  dataset_size = InData.dataset_size
-  test_set_size = InData.test_set_size
-  num_itr = InData.num_itr    
-  pre_idxs = np.empty(int(num_itr+1),dtype = int)    
-  for i in range(int(num_itr+1)):
-    pre_idxs[i] = InData.pre_idxs[i]
-  #-- Preprocess
-  preprocess = InData.preprocess
-  mbtr_red = InData.mbtr_red
-  #-- Path 
-  mbtr_path = InData.mbtr_path
-  json_path = InData.json_path
-  loadidxs_path = InData.loadidxs_path
-  loadsplitidxs_path = InData.loadsplitidxs_path
-  #-- Kernel
-  kernel_type = InData.kernel_type
-  length = InData.length
-  const = InData.const
-  bound = InData.bound
-  n_opt = InData.n_opt
-  alpha = InData.alpha
-  normalize_y = InData.normalize_y
-  #<<< End Loading config.
+  # #-- Output
+  # out_name = InData.out_name
+  # #-- Condition
+  # fn_name = InData.fn_name
+  # K_high = InData.K_high
+  # rnd_size = InData.rnd_size
+  # random_seed = InData.random_seed
+  # #- Flag
+  # save_load_flag = InData.save_load_flag
+  # save_load_split_flag = InData.save_load_split_flag
+  # restart_flag = InData.restart_flag
+  # #-- Data set
+  # dataset = InData.dataset
+  # dataset_size = InData.dataset_size
+  # test_set_size = InData.test_set_size
+  # num_itr = InData.num_itr    
+  # pre_idxs = np.empty(int(num_itr+1),dtype = int)    
+  # for i in range(int(num_itr+1)):
+  #   pre_idxs[i] = InData.pre_idxs[i]
+  # #-- Preprocess
+  # preprocess = InData.preprocess
+  # mbtr_red = InData.mbtr_red
+  # #-- Path 
+  # mbtr_path = InData.mbtr_path
+  # json_path = InData.json_path
+  # loadidxs_path = InData.loadidxs_path
+  # loadsplitidxs_path = InData.loadsplitidxs_path
+  # #-- Kernel
+  # kernel_type = InData.kernel_type
+  # length = InData.length
+  # const = InData.const
+  # bound = InData.bound
+  # n_opt = InData.n_opt
+  # alpha = InData.alpha
+  # normalize_y = InData.normalize_y
+  # #<<< End Loading config.
 
   # set the random seed
-  np.random.seed(random_seed)
+  np.random.seed(conf.random_seed)
 
   # #-- Initialize
   # if restart_flag:
@@ -131,60 +131,60 @@ def main():
   # else:
   #   overwrite(out_name, "")
 
-  append_write(out_name, datetime.datetime.today().strftime("%Y-%m-%dT%I-%M-%S"))
+  append_write(conf.out_name, datetime.datetime.today().strftime("%Y-%m-%dT%I-%M-%S"))
   #>>> Load Dataset
   #-- Load for descriptor
   start = time.time()
-  append_write(out_name, "start load mbtr \n")
-  mbtr_data = load_npz(mbtr_path)
+  append_write(conf.out_name, "start load mbtr \n")
+  mbtr_data = load_npz(conf.mbtr_path)
   process_time = time.time() - start
-  out_time(out_name, process_time)
+  out_time(conf.out_name, process_time)
 
   #-- Load for HOMO energy
-  if dataset in ["OE", "AA", "QM9"]:
+  if conf.dataset in ["OE", "AA", "QM9"]:
     df_62k = 0
     start = time.time()	
-    append_write(out_name, "start load OE \n")
-    homo_lowfid = np.loadtxt(json_path)
+    append_write(conf.out_name, "start load OE \n")
+    homo_lowfid = np.loadtxt(conf.json_path)
     process_time = time.time() - start
-    out_time(out_name, process_time)
+    out_time(conf.out_name, process_time)
   else:
-    append_write(out_name,"Dataset sould be AA or OE or QM9\n")
-    append_write(out_name,"program stopped ! \n")    
+    append_write(conf.out_name,"Dataset sould be AA or OE or QM9\n")
+    append_write(conf.out_name,"program stopped ! \n")    
     sys.exit()
 
   #-- Reduce the size of descriptor array
-  if mbtr_red :
+  if conf.mbtr_red :
     mbtr_data_red = mbtr_data[:,mbtr_data.getnnz(0) > 0]
   else:
     mbtr_data_red = mbtr_data
 
   #-- Output
-  out_condition(out_name, InData)
-  f = open(out_name, 'a')
+  out_condition(conf.out_name, conf)
+  f = open(conf.out_name, 'a')
   f.write("mbtr_data col " + str(mbtr_data.shape[0]) + "\n" )
   f.write("mbtr_data row " + str(mbtr_data.shape[1]) + "\n" )
   mbtr_data_size = mbtr_data.shape[0]
   f.write("mbtr_data_size " + str(mbtr_data_size) + "\n" )
   f.write("mbtr_data_red_size " + str(mbtr_data_red.shape[1]) + "\n" )
   f.write("=============================" + "\n")
-  append_write(out_name,str(0) + "-th learning \n")
+  append_write(conf.out_name,str(0) + "-th learning \n")
   f.flush()
   f.close()
 
   #-- Figure of 62k dataset and AA dataset
-  if dataset in ["OE", "QM9", "AA"] :
+  if conf.dataset in ["OE", "QM9", "AA"] :
       # fig_atom(df_62k,range(61489),"atoms_all.eps")
-      fig_HOMO(homo_lowfid,range(dataset_size), "HOMO_all.eps")
+      fig_HOMO(homo_lowfid,range(conf.dataset_size), "HOMO_all.eps")
   else:
-      append_write(out_name,"Dataset sould be AA, OE or QM9 \n")
-      append_write(out_name,"program stopped ! \n")    
+      append_write(conf.out_name,"Dataset sould be AA, OE or QM9 \n")
+      append_write(conf.out_name,"program stopped ! \n")    
       sys.exit()
   #<<< End loading data
 
   #>>> Setup the GP model
   n_features = int(mbtr_data_red.shape[1])
-  gpr = get_gpr(out_name, const, bound, length, kernel_type, normalize_y, n_opt, random_seed, alpha)
+  gpr = get_gpr(conf.out_name, conf.const, conf.bound, conf.length, conf.kernel_type, conf.normalize_y, conf.n_opt, conf.random_seed, conf.alpha)
   #<<< Finished setting up GP model
 
 
@@ -216,22 +216,23 @@ def main():
   # 7. Goto 2.
 
   #>>> Identify indices file from last training iteration.
-  for idx, batch_size in enumerate(pre_idxs[1:], 1):
+  for idx, batch_size in enumerate(conf.pre_idxs[1:], 1):
     # Try to open the full_idxs file, if you can, save the last rem, train, test.
     # If you cannot, use the latest rem train test indices to continue.
     try:
-      append_write(out_name, f"{idx}, {batch_size}\n")
-      data = np.load(f"{out_name}_{idx}_full_idxs.npz")
-      append_write(out_name, f"loaded {out_name}_{idx}_full_idxs.npz Continuing iteration.\n")
+      append_write(conf.out_name, f"{idx}, {batch_size}\n")
+      data = np.load(f"{conf.out_name}_{idx}_full_idxs.npz")
+      print("Loaded data.")
+      append_write(conf.out_name, f"loaded {conf.out_name}_{idx}_full_idxs.npz Continuing iteration.\n")
     except Exception as e:
-      append_write(out_name, f"iteration {idx} -- {e}, Couldn't load _full_idxs for index {idx}\n")
+      append_write(conf.out_name, f"iteration {idx} -- {e}, Couldn't load _full_idxs for index {idx}\n")
       break
       # So we don't have the full_idxs file corresponding to the latest idx.
       # the previous rem_idxs, pred_idxs, test_idxs are already loaded.
 
   idx = idx - 1
-  for idx, batch_size in enumerate(pre_idxs[idx:], idx):
-    append_write(out_name, f"Resuming from index {idx} and current batch size is {batch_size}\n")
+  for idx, batch_size in enumerate(conf.pre_idxs[idx:], idx):
+    append_write(conf.out_name, f"Resuming from index {idx} and current batch size is {batch_size}\n")
     rem_idxs  = data['remaining_idxs']
     pred_idxs = data['prediction_idxs']
     test_idxs = data['test_idxs']
@@ -241,36 +242,36 @@ def main():
     y_train, y_test = homo_lowfid[pred_idxs], homo_lowfid[test_idxs]
 
     # train GP with pred_idxs
-    X_train_pp, X_test_pp = desc_pp(preprocess, X_train, X_test)
+    X_train_pp, X_test_pp = desc_pp(conf.preprocess, X_train, X_test)
 
     const, length = get_gpr_params(gpr) 
-    append_write(out_name, f"length of RBF kernel before fitting {length} \n")
-    append_write(out_name, f"constant of constant kernel before fitting {const} \n")
+    append_write(conf.out_name, f"length of RBF kernel before fitting {length} \n")
+    append_write(conf.out_name, f"constant of constant kernel before fitting {const} \n")
 
     pdb.set_trace()
     try:
       # try to load gpr
-      gpr = joblib.load(f"{out_name}_{idx}_model.pkl")
-      append_write(out_name, f"Loaded {out_name}_{idx}_model.pkl \n")
+      gpr = joblib.load(f"{conf.out_name}_{idx}_model.pkl")
+      append_write(conf.out_name, f"Loaded {conf.out_name}_{idx}_model.pkl \n")
     except Exception as e:
       # if can't load train again
-      append_write(out_name, f"Can't load {out_name}_{idx}_model.pkl, retraining \n")
+      append_write(conf.out_name, f"Can't load {conf.out_name}_{idx}_model.pkl, retraining \n")
       gpr.fit(X_train_pp, y_train)
       # save GP model
-      joblib.dump(gpr, f"{out_name}_{idx}_model.pkl")
+      joblib.dump(gpr, f"{conf.out_name}_{idx}_model.pkl")
 
     const, length = get_gpr_params(gpr) 
-    append_write(out_name, f"length of RBF kernel after fitting {length} \n")
-    append_write(out_name, f"constant of constant kernel after fitting {const} \n")
+    append_write(conf.out_name, f"length of RBF kernel after fitting {length} \n")
+    append_write(conf.out_name, f"constant of constant kernel after fitting {const} \n")
 
-    append_write(out_name, "Finished training \n")
+    append_write(conf.out_name, "Finished training \n")
     process_time = time.time() - start
-    out_time(out_name, process_time)
+    out_time(conf.out_name, process_time)
 
     # Go through acq_fn to get new pred_idxs
     prediction_set_size = batch_size
     start = time.time()
-    append_write(out_name, f"Starting acq function.\n")
+    append_write(conf.out_name, f"Starting acq function.\n")
     pred_idxs, rem_idxs, X_train_pp, y_train = acq_fn(fn_name\
                                                       , idx\
                                                       , pred_idxs\
@@ -279,16 +280,16 @@ def main():
                                                       , rnd_size\
                                                       , mbtr_data_red\
                                                       , homo_lowfid\
-                                                      , K_high\
+                                                      , conf.K_high\
                                                       , gpr\
-                                                      , preprocess\
-                                                      , out_name\
-                                                      , random_seed)
-    append_write(out_name, f"Acq fun done.\n")
+                                                      , conf.preprocess\
+                                                      , conf.out_name\
+                                                      , conf.random_seed)
+    append_write(conf.out_name, f"Acq fun done.\n")
     process_time = time.time() - start 
-    out_time(out_name, process_time)
-    np.savez(f"{out_name}_{idx}_full_idxs.npz",remaining_idxs=rem_idxs, prediction_idxs = pred_idxs, test_idxs = test_idxs)
-    compute_stats(out_name, gpr, X_test_pp, y_test)
+    out_time(conf.out_name, process_time)
+    np.savez(f"{conf.out_name}_{idx}_full_idxs.npz",remaining_idxs=rem_idxs, prediction_idxs = pred_idxs, test_idxs = test_idxs)
+    compute_stats(conf.out_name, gpr, X_test_pp, y_test)
   #<<<
 
 if __name__=="__main__":
