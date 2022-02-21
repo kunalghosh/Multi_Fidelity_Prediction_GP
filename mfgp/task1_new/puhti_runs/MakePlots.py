e0f63b75a00d9f382faf3aeb9f6aa0ab45d94ede
Kunal Ghosh <kunal.t2@gmail.com>
2022-02-21 11:43:23 +0200

    ipython notebook for plots and corresponding py file created using jupytext

---
+++ mfgp/task1_new/puhti_runs/MakePlots.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.6
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# This notebook is used to make plots. The notebook is converted to a plain python text file using [jupytext](https://jupytext.readthedocs.io/en/latest/)
+
+# %%
