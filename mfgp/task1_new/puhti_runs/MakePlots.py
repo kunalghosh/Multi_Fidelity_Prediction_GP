4d9263988d375a42324b1e48c1aaaa974001440d
Kunal Ghosh <kunal.t2@gmail.com>
2022-02-25 22:20:10 +0200

    Wrote functions to compute parts of data savings plot.

--- mfgp/task1_new/puhti_runs/MakePlots.py
+++ mfgp/task1_new/puhti_runs/MakePlots.py
@@ -29,7 +29,8 @@ aa_a_1k.mean_vals
 # %%
 os.chdir("/projappl/project_2000382/ghoshkun/code/Multi_Fidelity_Prediction_GP/mfgp/task1_new/puhti_runs")
 print(os.getcwd())
-# !module load python-data/3.7.
+# !module load python-data/3.7.6-1
+# !python -m pip install jupytext
 # !python -m jupytext --sync MakePlots.ipynb
 
 # %% [markdown]
@@ -145,53 +146,134 @@ plot_strategy_a_vs_d(dataset_name = "AA", means=means, stds=stds)
 # %%
 from scipy.optimize import curve_fit
 
-coeffs = { 'A' : None, 'D' : None }
-mean_dict = {'A' : None, 'D' : None}
+def fit_curve_to_data(means, stds, labels, batch_sizes):
+    coeffs = { 'A' : None, 'D' : None }
+    mean_dict = {'A' : None, 'D' : None}
+    for idx, (mean, std) in enumerate(zip(means,stds)):
+        if labels[idx] in ['A', 'D']:
+            """
+            Only plot strategy A and D.
+            """
+            print(f"Data for strategy {labels[idx]}")
 
-for idx, (mean, std) in enumerate(zip(means,stds)):
-    if labels[idx] in ['A', 'D']:
-        """
-        Only plot strategy A and D.
-        """
-        print(f"Data for strategy {labels[idx]}")
+            results = curve_fit(lambda x,a,b,c,d: d + (a-d)/(1 + (x/c)**b), batch_sizes, mean, p0=[0.5, 0.5, 0.5, 0.5], bounds=(-1, [3., 1., 2, 2]))
+            a, b, c, d= results[0]
+            coeffs[labels[idx]] = results[0]
+            mean_dict[labels[idx]] = mean
+            print(a, b, c, d)
+            x = batch_sizes # np.arange(0, 17, batch_size)
+            y = d + (a-d)/(1 + (x/c)**b)
 
-        results = curve_fit(lambda x,a,b,c,d: d + (a-d)/(1 + (x/c)**b), batch_sizes, mean, p0=[0.5, 0.5, 0.5, 0.5], bounds=(-1, [3., 1., 2, 2]))
-        a, b, c, d= results[0]
-        coeffs[labels[idx]] = results[0]
-        mean_dict[labels[idx]] = mean
-        print(a, b, c, d)
-        x = batch_sizes # np.arange(0, 17, batch_size)
-        y = d + (a-d)/(1 + (x/c)**b)
-        
-        plt.plot(x, y)
-        plt.scatter(batch_sizes, mean, label=labels[idx])
-plt.legend()
+            plt.plot(x, y)
+            plt.scatter(batch_sizes, mean, label=labels[idx])
+    plt.legend()
+    return coeffs, mean_dict
+
+
+# fit_curve_to_data(means, stds, labels, batch_sizes)
 
 # %%
-xticks = [_ for _ in range(0,17,batch_size)]
-data_saving = []
-a,b,c,d = coeffs['D']
-for y, batch_size in zip(mean_dict['A'], batch_sizes):
-    x = c * (-1 + (a-d)/(y-d))**(1./b)
-    xticks.append(x)
-    data_saving.append(batch_size - x)
-    print(batch_size, x)
-    # vertical line at x
-    # plt.axvline(x=x, color='k', linestyle="--")
-    plt.plot((x, x), (0, y), color='k', linestyle="--")
-    plt.hlines(y, x, batch_size, color='k', linestyle="--")
+def get_and_plot_data_savings(mean_dict, coeffs, batch_size, batch_sizes):
+    xticks = [_ for _ in range(0,17,batch_size)]
+    data_saving = []
+    a,b,c,d = coeffs['D']
+    for y, batch_size in zip(mean_dict['A'], batch_sizes):
+        x = c * (-1 + (a-d)/(y-d))**(1./b)
+        xticks.append(x)
+        data_saving.append(batch_size - x)
+        print(batch_size, x)
+        # vertical line at x
+        # plt.axvline(x=x, color='k', linestyle="--")
+        plt.plot((x, x), (0, y), color='k', linestyle="--")
+        plt.hlines(y, x, batch_size, color='k', linestyle="--")
 
-xticks.sort()
-print(xticks)
-plt.legend()
-xticks_str = []
-for idx, x in enumerate(xticks):
-    if x in batch_sizes:
-        xticks_str.append(" ")
-    else:
-        xticks_str.append("%.2f" % x)
+    xticks.sort()
+    print(xticks)
+    plt.legend()
+    xticks_str = []
+    for idx, x in enumerate(xticks):
+        if x in batch_sizes:
+            xticks_str.append(" ")
+        else:
+            xticks_str.append("%.2f" % x)
+    
+    print(xticks_str)
+    return data_saving
 
-print(xticks_str)
+# get_and_plot_data_savings(mean_dict, coeffs, batch_size, batch_sizes)
+
+
+# %%
+import os
+import pandas as pd
+import numpy as np
+import matplotlib
+# %matplotlib inline
+from matplotlib import pyplot as plt
+
+def plot_data_savings(strategy_a_file, strategy_d_file, batch_size):
+    aa_a = pd.read_csv(strategy_a_file)
+    aa_d = pd.read_csv(strategy_d_file)
+    
+    # formatting plots
+    font = {'family' : 'normal', 'size'   : 15}
+    lines = {"linewidth": 4}
+
+    matplotlib.rc('font', **font)
+    matplotlib.rc('lines', **lines)
+
+    batch_size = batch_size # 1 for 1k, 2 for 2k etc
+    batch_sizes = np.arange(0,17,batch_size) + 1
+    
+    labels = "A B C D E F G".split(" ")
+    labs = [[_] for _ in labels]
+
+    aa_a_means = aa_a.mean_vals.to_numpy()
+    aa_d_means = aa_d.mean_vals.to_numpy()
+
+    aa_a_stds = aa_a.std_vals.to_numpy()
+    aa_d_stds = aa_d.std_vals.to_numpy()
+
+
+    stds = [aa_a_stds,
+         [],
+         [],
+         aa_d_stds,
+         [],
+         [],
+         []]
+
+    means = [aa_a_means,
+         [],
+         [],
+         aa_d_means,
+         [],
+         [],
+         []]
+    
+    coeffs, mean_dict = fit_curve_to_data(means, stds, labels, batch_sizes)
+    data_saving = get_data_savings(mean_dict, coeffs, batch_size, batch_sizes)
+    
+    data_saving = np.array(data_saving)
+    batch_sizes = np.array(batch_sizes)
+    print(f"Batch sizes {batch_sizes}")
+    savings_in_percent = 100 * data_saving / batch_sizes
+    print(f"datasavings {data_saving}")
+    plt.plot(batch_sizes, savings_in_percent, color="k")
+    print(f"datasavings {savings_in_percent}")
+    plt.scatter(batch_sizes, savings_in_percent, color="k", label="Datasaving (D vs A) in percent")
+    plt.xscale("linear")
+    plt.legend()
+    plt.xticks(batch_sizes)
+    plt.xlabel("Data size (x10^3)")
+    plt.ylabel("Datasaving in percent")
+    plt.grid()
+    
+
+os.chdir("/projappl/project_2000382/ghoshkun/code/Multi_Fidelity_Prediction_GP/mfgp/task1_new/puhti_runs")    
+plot_data_savings(strategy_a_file = "csv_files/Active_learning_results - AA_A_1k.csv",
+                  strategy_d_file = "csv_files/Active_learning_results - AA_D_1k.csv",
+                  batch_size=1)
 
 # %%
 data_saving = np.array(data_saving)
