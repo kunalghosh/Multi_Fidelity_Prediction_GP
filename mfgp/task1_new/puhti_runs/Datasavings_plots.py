3554689ba3f151190db1536fcf7e94dbba205274
Kunal Ghosh <kunal.t2@gmail.com>
2022-06-13 17:24:36 +0300

    Updated notebooks

--- mfgp/task1_new/puhti_runs/Datasavings_plots.py
+++ mfgp/task1_new/puhti_runs/Datasavings_plots.py
@@ -69,12 +69,11 @@ def fit_data(df_a, fn):
 def compute_rmse(pred, true):
     return np.mean((pred-true)**2)
 
-def fit_data_and_check(path_to_csv):
+def fit_data_and_check(fn, path_to_csv):
     df = pd.read_csv(path_to_csv)
     x = df.train_size//1000
     y = df.mean_vals
     
-    fn = lambda x,a,b,c: -a + c/x**b # hyperbolic function being fit to data 
     fit_obj = scipy_fit(fn, x, y)
     fit_params = fit_obj.x
     
@@ -90,6 +89,23 @@ def fit_data_and_check(path_to_csv):
     
     return y_pred, fit_params, x, y
 
+# functions to fit
+fn = lambda x,a,b,c: -a + c/x**b # hyperbolic function being fit to data 
+fn_inv = lambda y,a,b,c: (c/(y+a))**(1/b)
+
+def compute_datasavings(x_d, y_d, fn_inv, fit_params):
+    # given mae for d (y_d) compute corresponding data required by strategy_a (x_a)
+    xa_given_mae = fn_inv(y_d, *fit_params)
+    
+    # How many more datapoints (in 1000s) does strategy_a require ? to get same mae
+    data_diff = xa_given_mae - x_d
+    print(data_diff)
+    
+    # data_savings in percent
+    savings = 100 * data_diff / x_d
+    
+    return savings
+
 
 # %%
 import os
@@ -101,24 +117,24 @@ os.chdir("/projappl/project_2000382/ghoshkun/code/Multi_Fidelity_Prediction_GP/m
 
 # %%
 # computations for AA
-y_predict_aa, fit_params_aa, x, y_true_aa = fit_data_and_check("csv_files/old_csvs/Active_learning_results - AA_A_1k.csv")
+y_predict_aa, fit_params_aa, x_aa, y_true_aa = fit_data_and_check(fn, "csv_files/Active_learning_results - AA_A_1k.csv")
 
-aa_d = pd.read_csv("csv_files/old_csvs/Active_learning_results - AA_D_1k.csv")
-aa_vals = (y_true_aa, aa_d.mean_vals, y_predict_aa, x)
+aa_d = pd.read_csv("csv_files/Active_learning_results - AA_D_1k.csv")
+aa_vals = (y_true_aa, aa_d.mean_vals, y_predict_aa, x_aa)
 
 # %%
 # Computations for QM9
-y_predict_qm9, fit_params_qm9, x, y_true_qm9 = fit_data_and_check("csv_files/old_csvs/Active_learning_results - QM9_A_EXP.csv")
+y_predict_qm9, fit_params_qm9, x_qm9, y_true_qm9 = fit_data_and_check(fn, "csv_files/Active_learning_results - QM9_A_EXP.csv")
 
-qm9_d = pd.read_csv("csv_files/old_csvs/Active_learning_results - QM9_D_EXP.csv")
-qm9_vals = (y_true_qm9, qm9_d.mean_vals, y_predict_qm9, x)
+qm9_d = pd.read_csv("csv_files/Active_learning_results - QM9_D_EXP.csv")
+qm9_vals = (y_true_qm9, qm9_d.mean_vals, y_predict_qm9, x_qm9)
 
 # %%
 # Computations for OE
-y_predict_oe, fit_params_oe, x, y_true_oe = fit_data_and_check("csv_files/old_csvs/Active_learning_results - OE_A_EXP.csv")
+y_predict_oe, fit_params_oe, x_oe, y_true_oe = fit_data_and_check(fn, "csv_files/Active_learning_results - OE_A_EXP.csv")
 
-oe_d = pd.read_csv("csv_files/old_csvs/Active_learning_results - OE_D_EXP.csv")
-oe_vals = (y_true_oe, oe_d.mean_vals, y_predict_oe, x)
+oe_d = pd.read_csv("csv_files/Active_learning_results - OE_D_EXP.csv")
+oe_vals = (y_true_oe, oe_d.mean_vals, y_predict_oe, x_oe)
 
 
 # %% [markdown]
@@ -163,5 +179,130 @@ def plot(aa_vals, oe_vals, qm9_vals):
     plt.grid()
 
 plot(aa_vals, oe_vals, qm9_vals)
+
+# %%
+# aa_d_exp = pd.read_csv("csv_files/Active_learning_results - AA_D_EXP.csv")
+# batch_size_aa = aa_d_exp.train_size//1000
+# x_aa = batch_size_aa
+# computations for AA
+
+savings_aa = compute_datasavings(x_aa, aa_d.mean_vals, fn_inv, fit_params_aa)
+savings_oe = compute_datasavings(x_oe, oe_d.mean_vals, fn_inv, fit_params_oe)
+savings_qm9 = compute_datasavings(x_qm9, qm9_d.mean_vals, fn_inv, fit_params_qm9)
+
+def plot_datasavings_qm9_aa_oe(batch_size_qm9,savings_qm9,batch_size_aa,savings_aa,batch_size_oe,savings_oe):
+    set_mpl_params_meeting(matplotlib)
+    
+    batch_size_qm9 = batch_size_qm9
+    savings_qm9 = savings_qm9
+    plt.plot(batch_size_qm9, savings_qm9, color="#1b9e77")
+    plt.scatter(batch_size_qm9, savings_qm9, color="#1b9e77", label="QM9")
+
+    plt.plot(batch_size_aa, savings_aa, color="k")
+    plt.scatter(batch_size_aa, savings_aa, color="k", label="AA")
+    
+    plt.plot(batch_size_oe, savings_oe, color="tab:orange")
+    plt.scatter(batch_size_oe, savings_oe, color="tab:orange", label="OE")
+
+    # plt.xscale("linear")
+    plt.legend()
+    #plt.xticks(batch_sizes)
+    plt.xlabel("Training set size (in 1000s)")
+    plt.ylabel("Datasaving in percent")
+    plt.grid()
+    plt.savefig("DataSavings_fitonlyfor_a.png")
+
+plot_datasavings_qm9_aa_oe(x_qm9,savings_qm9,x_aa,savings_aa,x_oe,savings_oe)
+
+# %%
+# aa_d_exp = pd.read_csv("csv_files/Active_learning_results - AA_D_EXP.csv")
+# batch_size_aa = aa_d_exp.train_size//1000
+# x_aa = batch_size_aa
+
+y_predict_aa, fit_params_aa, x_aa, y_true_aa = fit_data_and_check(fn, "csv_files/Active_learning_results - AA_A_EXP.csv")
+aa_d = pd.read_csv("csv_files/Active_learning_results - AA_D_EXP.csv")
+aa_vals = (y_true_aa, aa_d.mean_vals, y_predict_aa, x_aa)
+
+savings_aa = compute_datasavings(x_aa, aa_d.mean_vals, fn_inv, fit_params_aa)
+savings_oe = compute_datasavings(x_oe, oe_d.mean_vals, fn_inv, fit_params_oe)
+savings_qm9 = compute_datasavings(x_qm9, qm9_d.mean_vals, fn_inv, fit_params_qm9)
+
+def plot_datasavings_qm9_aa_oe(batch_size_qm9,savings_qm9,batch_size_aa,savings_aa,batch_size_oe,savings_oe):
+    set_mpl_params_meeting(matplotlib)
+    
+    batch_size_qm9 = batch_size_qm9
+    savings_qm9 = savings_qm9
+    plt.plot(batch_size_qm9, savings_qm9, color="#1b9e77")
+    plt.scatter(batch_size_qm9, savings_qm9, color="#1b9e77", label="QM9")
+
+    plt.plot(batch_size_aa, savings_aa, color="k")
+    plt.scatter(batch_size_aa, savings_aa, color="k", label="AA")
+    
+    plt.plot(batch_size_oe, savings_oe, color="tab:orange")
+    plt.scatter(batch_size_oe, savings_oe, color="tab:orange", label="OE")
+
+    # plt.xscale("linear")
+    plt.legend()
+    #plt.xticks(batch_sizes)
+    plt.xlabel("Training set size (in 1000s)")
+    plt.ylabel("Datasaving in percent")
+    plt.grid()
+    plt.savefig("DataSavings_fitonlyfor_a.png")
+
+plot_datasavings_qm9_aa_oe(x_qm9,savings_qm9,x_aa,savings_aa,x_oe,savings_oe)
+
+# %% [markdown]
+# # Check the data fit before
+# 1. 200 percent data savings looks too good to be true, but it looks plausible looking at the data (plot A vs D)
+# 2. Check how was the fit using the old fit function.
+# 3. The fit for OE_A looks particularly bad, can I run a simple code which checks this again quickly ? GPyTorch, Strategy A
+# 4. After strategy A has been implemented, Maybe Implement Strategy D as well just to double check that data savings are as good as they seem indeed.
+#
+
+# %%
+# change scipy curve fit to include 4th parameter
+fn = lambda x,a,b,c,d: d + (a-d)/(1 + (x/c)**b)
+y_predict_qm9, fit_params_qm9, x_qm9, y_true_qm9 = fit_data_and_check(fn, "csv_files/Active_learning_results - AA_A_EXP.csv")
+
+# %% [markdown]
+# Using the old approximating function, we get a similarly good fit. Seems that the fitting function sklearn `curve_fit` couldn't fit the data well. We will check that next.
+
+# %%
+import numpy as np
+from scipy.optimize import curve_fit
+
+def get_fit_obj(fn, x, y):
+    def fx(args):
+        x = np.array(x)
+        predict = fn(x, *args)
+        true =  y
+        error = np.mean((predict - true)**2)
+        return error
+    # params = minimize(fx, (0.7, 2, 0.55,0.5)).x
+    params = curve_fit(fn, x, y, p0=[0.5, 0.5, 0.5, 0.5], bounds=(-1, [1., 1., 2, 2]), maxfev=1000)[0]
+    return params
+
+_fn = lambda x,a,b,c,d: d + (a-d)/(1 + (x/c)**b)
+
+def fit_data_and_check(fn, path_to_csv):
+    df = pd.read_csv(path_to_csv)
+    x = df.train_size//1000
+    y = df.mean_vals
+    
+    fit_params = get_fit_obj(fn, x, y)
+    print(fit_params)
+    y_pred = get_y(fn, fit_params, x)
+    
+    # compute errors for quantitative check
+    rmse = compute_rmse(y_pred, y)
+    r2   = np.corrcoef(y_pred, y)
+    print(f"rmse : {rmse},\nr2 :\n {r2}")
+    
+    # plot the fit and true data for visual checks
+    plot_fit_and_true(y_pred, y, x)
+    
+    return y_pred, fit_params, x, y
+
+y_predict_qm9, fit_params_qm9, x_qm9, y_true_qm9 = fit_data_and_check(_fn, "csv_files/Active_learning_results - AA_D_EXP.csv")
 
 # %%
