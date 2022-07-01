59b3017d1b06d506806e9796d7f39f767e571a70
Kunal Ghosh <kunal.ghosh@aalto.fi>
2022-07-01 14:20:44 +0300

    Removed more unused files

--- mfgp/__init__.py
+++ mfgp/__init__.py
@@ -1,2 +1,3 @@
-# from task_1_new import the function which gets exposed to the user.
-# then user would call it mfgp import function
+from . import acq
+from . import io_utils
+
--- mfgp/task1_new/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .helpers import xyz2ase, get_level
