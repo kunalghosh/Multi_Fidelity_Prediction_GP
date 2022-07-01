1e4d1d409e0a6b93afbcb7054e8af480624b5397
Kunal Ghosh <kunal.t2@gmail.com>
2022-07-01 19:29:46 +0300

    In older input.dat files range_low did not exist, handled that

--- mfgp_refactor/utils/summary.py
+++ mfgp_refactor/utils/summary.py
@@ -3,6 +3,7 @@ import click
 import numpy as np
 import glob
 
+default_range_low = {"AA":-8.5, "OE":-5.55, "QM9":-5.2}
 
 @click.command()
 @click.argument("working_dir", default=".")
@@ -13,10 +14,15 @@ def main(idxs_within_energy, working_dir):
     if idxs_within_energy:
         config = Input(f"{working_dir}/input.dat")
         homo_vals = np.loadtxt(config.json_path)
-        range_low = float(config.range_low)
+        
+        range_low = config.range_low
+        if range_low is None:
+            range_low = default_range_low[config.dataset]
+        range_low = float(range_low)
+    
         max_num_above_range = sum(homo_vals > range_low)
         print(
-            f"{config.json_path} has {max_num_above_range} values above {config.range_low} eV"
+            f"{config.json_path} has {max_num_above_range} values above {range_low} eV"
         )
         full_idxs_files = glob.glob(f"{working_dir}/*_full_idxs*")
         sorted_idxs_files = sorted(full_idxs_files, key=lambda x:int(x.split("_")[-3]))
