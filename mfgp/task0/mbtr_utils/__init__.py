6053ac87b1b546c87b72d83680b2ce03d0ba47f6
Kunal Ghosh <kunal.ghosh@aalto.fi>
2022-07-01 14:17:54 +0300

    Moved files around and deleted unused files

--- mfgp/task0/mbtr_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .create import create_mbtr
-from .mbtr_description import make_mbtr_desc
--- mfgp/task1/run_gp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-cd $WRKDIR/MBTR_62k_GEN/Multi_Fidelity_Prediction_GP/mfgp/task1
-mkdir build || true
-cd build
-module load CUDA/9.0.176.lua || true
-module load anaconda3 || true
-source activate ../../task0/build_mbtr/myenv
-# python ../GP_prediction_low_fidelity_ExactGPyTorch.py ../../task0/mbtr_data_flattened/mbtr.npz ../../task0/build_mbtr/df_full_split_ids_with_smiles_v15.json
-python ../GP_prediction_low_fidelity_ExactGPyTorch.py ../../task0/build_mbtr/mbtrk2.npz ../../task0/build_mbtr/df_full_split_ids_with_smiles_v15.json
