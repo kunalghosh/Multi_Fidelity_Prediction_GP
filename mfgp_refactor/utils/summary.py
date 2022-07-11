a230b03aa205896cb9b02502cf649cd3761f3013
Kunal Ghosh <kunal.ghosh@aalto.fi>
2022-07-11 22:44:47 +0300

    Added metrics

--- mfgp_refactor/utils/summary.py
+++ mfgp_refactor/utils/summary.py
@@ -6,6 +6,47 @@ import glob
 default_range_low = {"AA": -8.5, "OE": -5.2, "QM9": -5.55}
 
 
+def get_true_positive(idx, num_in_range, predicted_in_range, percent=True):
+    """
+    How many were `in range` out of the total `predicted to be in range`
+    """
+    true_positive = None
+    if idx > 0:
+        # because iteration 0 is randomly picked
+        true_positive = num_in_range / predicted_in_range
+
+        if percent:
+            true_positive = true_positive * 100
+
+    return true_positive
+
+
+def get_full_idxs_filenames(working_dir):
+    full_idxs_files = glob.glob(f"{working_dir}/*_full_idxs*")
+    sorted_idxs_files = sorted(full_idxs_files, key=lambda x: int(x.split("_")[-3]))
+    return sorted_idxs_files
+
+
+def get_predicted_homos(idx):
+    files = glob.glob(f"*{idx}_testset_predictions.npz.npy")
+    try:
+        predicted_vals = np.load(files[0])
+    except Exception as e:
+        predicted_vals = None
+    return predicted_vals
+
+
+def get_mae(test_homos, predicted_homos, range_low):
+    error_overall = np.abs(test_homos - predicted_homos)
+    mask_test_homos_in_range = test_homos > range_low  # mask of test_homos in range
+    print(test_homos[mask_test_homos_in_range])
+    print(error_overall)
+    print(mask_test_homos_in_range)
+    error_in_range = error_overall[mask_test_homos_in_range]
+    print(error_in_range)
+    return np.mean(error_overall), np.mean(error_in_range)
+
+
 @click.command()
 @click.argument("working_dir", default=".")
 @click.option(
@@ -17,26 +58,38 @@ def main(idxs_within_energy, working_dir):
         homo_vals = np.loadtxt(config.json_path)
 
         range_low = config.range_low
-        print(
-            "WARNING : Can't find range_low value in config. Using default range_low values."
-        )
         if range_low is None:
+            print(
+                "WARNING : Can't find range_low value in config. Using default range_low values."
+            )
             range_low = default_range_low[config.dataset]
         range_low = float(range_low)
 
         max_num_above_range = sum(homo_vals > range_low)
         print(f"{config.json_path} has {max_num_above_range} values above {range_low} eV")
-        full_idxs_files = glob.glob(f"{working_dir}/*_full_idxs*")
-        sorted_idxs_files = sorted(full_idxs_files, key=lambda x: int(x.split("_")[-3]))
-        for file in sorted_idxs_files:
+
+        sorted_idxs_files = get_full_idxs_filenames(working_dir)
+
+        for idx, file in enumerate(sorted_idxs_files):
             idxs_ = np.load(file)["prediction_idxs"]
+            test_idxs_ = np.load(file)["test_idxs"]
+
+            predicted_homos = get_predicted_homos(idx)
+            if predicte_homos is not None:
+                mae, mae_in_range = get_mae(
+                    homo_vals[test_idxs_], predicted_homos, range_low=range_low
+                )
+            else:
+                mae, mae_in_range = None, None
+
             num_above_range = sum(homo_vals[idxs_] > range_low)
+            true_positive = get_true_positive(idx, num_above_range, batch_size)
             assert (
                 num_above_range < max_num_above_range
             ), f"The number above {config.range_low} cannot be above the {max_num_above_range} calculated from all the homo values"
 
             print(
-                f" For file {file} above {config.range_low} eV = {num_above_range}, % of total = {num_above_range * 100 / max_num_above_range}"
+                f" For file {file} above {config.range_low} eV = {num_above_range}, % of total = {num_above_range * 100 / max_num_above_range}, test_MAE = {mae}, inRange_MAE = {mae_in_range}"
             )
 
 
