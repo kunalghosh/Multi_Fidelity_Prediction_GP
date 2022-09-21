ab6272c7600c7af74ed8df8aba214715b97f1658
Kunal Ghosh <kunal.t2@gmail.com>
2022-09-21 21:54:46 +0300

    resolving merge conflict

--- mfgp_refactor/utils/summary.py
+++ mfgp_refactor/utils/summary.py
@@ -12,7 +12,7 @@ default_range_low = {"AA": -8.5, "OE": -5.2, "QM9": -5.55}
 def fmt(val):
     try:
         if isinstance(val, np.int64):
-            a = f"{val:>9d}"
+            a = f"{val:>6d}"
         else:
             a = f"{val:>9.3f}"
     except Exception as e:
